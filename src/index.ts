d152205d430db70ca1d2a2d1ccfc22359637d7c5
ranger-ross <rosssullivan101@gmail.com>
2024-04-07 22:33:56 +0900

    added a parse major version func

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-console.log(123)
+function main() {
+
+}
+
+function parseMajorVersion(version: string): number {
+    let normalizedVersion = version
+    if (version[0] === 'v') {
+        normalizedVersion = version.substring(1)
+    }
+
+    let majorVersion = ''
+
+    for (const char in normalizedVersion.split('')) {
+        if (char === '.') {
+            break;
+        }
+
+        if (isNaN(parseInt(char))) {
+            throw Error("Could not parse major version from tag name");
+        }
+        majorVersion += char;
+    }
+
+    if (majorVersion) {
+        throw Error("Could not parse major version from tag name");
+    }
+
+    return parseInt(majorVersion)
+}
+
+
+
+main()
