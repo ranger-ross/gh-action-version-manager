86e0557ce28001aa46ab7f1df57c4bb6b3cda326
ranger-ross <rosssullivan101@gmail.com>
2024-04-07 22:41:48 +0900

    just pushing it

--- dist/index.js
+++ dist/index.js
@@ -1,35 +1,26 @@
-import * as core from '@actions/core';
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 function main() {
-    console.log('FOUND')
+    console.log('FOUND');
 }
-
-function parseMajorVersion(version: string): number {
-    let normalizedVersion = version
+function parseMajorVersion(version) {
+    var normalizedVersion = version;
     if (version[0] === 'v') {
-        normalizedVersion = version.substring(1)
+        normalizedVersion = version.substring(1);
     }
-
-    let majorVersion = ''
-
-    for (const char in normalizedVersion.split('')) {
+    var majorVersion = '';
+    for (var char in normalizedVersion.split('')) {
         if (char === '.') {
             break;
         }
-
         if (isNaN(parseInt(char))) {
             throw Error("Could not parse major version from tag name");
         }
         majorVersion += char;
     }
-
     if (majorVersion) {
         throw Error("Could not parse major version from tag name");
     }
-
-    return parseInt(majorVersion)
+    return parseInt(majorVersion);
 }
-
-
-
-main()
+main();
